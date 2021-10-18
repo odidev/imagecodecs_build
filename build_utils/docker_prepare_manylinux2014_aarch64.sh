f2b1a597c772fd90abe3d4b2b5538ca2a6f4bbbf
odidev <odidev@puresoftware.com>
2021-10-18 16:31:45 +0530

    Update docker_prepare_manylinux2014_aarch64.sh

--- build_utils/docker_prepare_manylinux2014_aarch64.sh
+++ build_utils/docker_prepare_manylinux2014_aarch64.sh
@@ -4,10 +4,15 @@ set -e
 yum install -y epel-release
 yum install -y pcre pcre-devel openssl-devel wget libtool snappy-devel gettext gettext-devel po4a nasm meson cmake3 ninja-build
 
-# On aarch64 machine, yum pull meson==0.47.0 and project requires meson >= 0.49.0. Installing through pip
+# On aarch64 machine, yum pull meson==0.47.0 and project requires meson >= 0.49.0 so installing it from source code
+wget https://github.com/mesonbuild/meson/releases/download/0.49.0/meson-0.49.0.tar.gz
+tar -xvzf meson-0.49.0.tar.gz
+cd meson-0.49.0
 export PATH=/opt/python/cp38-cp38/bin/:$PATH
-pip install meson==0.49.0
+python setup.py install
+cd ..
+rm -rf meson-0.49.0 meson-0.49.0.tar.gz
 
-ln -sf /opt/python/cp38-cp38/bin/meson /usr/bin/meson
-ln -s /usr/bin/cmake3 /usr/bin/cmake
-ln -s /usr/bin/ninja-build /usr/bin/ninja
+cp /opt/python/cp38-cp38/bin/meson /usr/bin/meson
+cp /usr/bin/cmake3 /usr/bin/cmake
+cp /usr/bin/ninja-build /usr/bin/ninja
