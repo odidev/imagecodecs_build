73e954ddbfa4f8387c6e3c0cdd62b3a8bee3acc8
odidev <odidev@puresoftware.com>
2021-10-18 16:28:35 +0530

    Update docker_prepare_manylinux2014_aarch64.sh

--- build_utils/docker_prepare_manylinux2014_aarch64.sh
+++ build_utils/docker_prepare_manylinux2014_aarch64.sh
@@ -2,12 +2,12 @@
 set -e
 
 yum install -y epel-release
-yum install -y pcre pcre-devel openssl-devel wget libtool snappy-devel gettext gettext-devel po4a nasm cmake3 ninja-build
+yum install -y pcre pcre-devel openssl-devel wget libtool snappy-devel gettext gettext-devel po4a nasm meson cmake3 ninja-build
 
 # On aarch64 machine, yum pull meson==0.47.0 and project requires meson >= 0.49.0. Installing through pip
 export PATH=/opt/python/cp38-cp38/bin/:$PATH
 pip install meson==0.49.0
 
-ln -s /opt/python/cp38-cp38/bin/meson /usr/bin/meson
+ln -sf /opt/python/cp38-cp38/bin/meson /usr/bin/meson
 ln -s /usr/bin/cmake3 /usr/bin/cmake
 ln -s /usr/bin/ninja-build /usr/bin/ninja
